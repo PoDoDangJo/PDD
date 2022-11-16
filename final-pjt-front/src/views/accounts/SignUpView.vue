ab78f914cc4b1dc77ab2938d2b2124c10dcd4d41
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-11-17 00:44:25 +0900

    [F001] Create files

---
+++ final-pjt-front/src/views/accounts/SignUpView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Sing Up</h1>
+    <form @submit.prevent="signUp">
+      <input type="text" placeholder="username" v-model="username" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUpView",
+};
+</script>
