4c819422f870e76c6b8c84dd704e32ab0c0962e4
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-11-17 00:43:11 +0900

    [F000] Update App

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,77 +1,52 @@
 <template>
   <div id="app">
-    <div dir="ltr" class="extended-diacritics-language">
-      <div class="bd dark-background">
-        <TheNavbarVue/>
-        <router-view/>
-      </div>
-    </div>
+    <TheHeader />
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-import TheNavbarVue from './components/TheNavbar.vue';
+import TheHeader from "@/components/TheHeader/TheHeader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    TheNavbarVue,
+    TheHeader,
   },
   methods: {
     // 영화 데이터 가져오는 함수
     getMovies() {
-      this.$store.dispatch('getMovies')
-    }
+      this.$store.dispatch("getMovies");
+    },
+    scroll() {
+      // 무한 스크롤
+      window.onscroll = () => {
+        let bottomOfWindow =
+          Math.max(
+            window.pageYOffset,
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+          ) +
+            window.innerHeight ===
+          document.documentElement.offsetHeight;
+        if (bottomOfWindow) {
+          this.scrolledToBottom += 1;
+        }
+      };
+    },
   },
   created() {
     // App 실행시 영화 데이터 가져오는 함수 실행
-    this.getMovies()
-  }
-}
+    this.getMovies();
+  },
+};
 </script>
 
 <style>
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+a {
+  text-decoration: none;
   color: #141414;
-  background: #FFF;
-  width: 100%;
-  height: 3240px;
 }
-
-@media screen and (min-width: 481px) and (max-width: 840px) and (max-aspect-ratio: 4 / 3) {
-  body, html {
-    font-size: 1.8vw;
-  }
-}
-
-body, html {
-  -webkit-font-smoothing: antialiased;
-  color: #fff;
-  cursor: default;
-  font-family: Helvetica, Arial, sans-serif;;
-  line-height: 1.2;
-  user-select: none;
-}
-
-/* 스크롤바 안 보이게 */
-::-webkit-scrollbar {
-  display: none;
-}
-
-.extended-diacritics-language {
-  line-height: 1.4;
-}
-
-.bd {
-  overflow: hidden;
-  z-index: 0;
-}
-
-.dark-background {
-  background: #fff;
-}
-
-
 </style>
