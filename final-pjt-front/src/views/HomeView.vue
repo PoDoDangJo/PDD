c63f91b3c8297eeacfacdd08facb58a0c5067d37
임성빈 <95527804+LimSB-dev@users.noreply.github.com>
2022-11-17 00:42:45 +0900

    [F001] Update HomeView

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <MovieList :msg="'최근 개봉 영화'"/>
+    <MovieList :msg="'최근 개봉 영화'" />
+    <MovieList :msg="'인기 영화'" />
+    <MovieList :msg="'추천 영화'" />
+    <MovieList :msg="'최근 개봉 영화'" />
   </div>
 </template>
 
 <script>
-import MovieList from '@/components/MovieList'
+import MovieList from "@/components/MovieList/MovieList";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     MovieList,
   },
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
