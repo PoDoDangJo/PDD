e5c835fe37909887b8d8ca722f0f07de56354e4b
Vailish <joyksj94@gmail.com>
2022-11-16 10:29:59 +0900

    [B000] Update_community&movies_app

--- final-pjt-back/pdd_api/settings.py
+++ final-pjt-back/pdd_api/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'articles',
     'accounts',
+    'community',
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
