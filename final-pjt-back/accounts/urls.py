9761725f361aaaf4527440e9a534569ea84d5833
Vailish <joyksj94@gmail.com>
2022-11-17 10:43:45 +0900

    [B000] Create Swagger

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -3,11 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.movie_list),
-    path('movie/<int:movie_pk>/', views.movie_detail),
-    path('comments/', views.comment_list),
-    path('comments/<int:comment_pk>/', views.comment_detail),
-    path('movie/<int:movie_pk>/comments/', views.comment_create),
+    path('<int:user_pk>/follow/', views.follow, name='follow'),
+    # swagger settings
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
