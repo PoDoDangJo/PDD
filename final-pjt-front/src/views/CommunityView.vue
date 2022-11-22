2c8cfa7e486b78aea7bcd28e959cdc6565954f48
LimSB-dev <mae03087@naver.com>
2022-11-22 17:45:38 +0900

    [F001] Update Views

--- final-pjt-front/src/views/CommunityView.vue
+++ final-pjt-front/src/views/CommunityView.vue
@@ -10,6 +10,7 @@
 import TheHeader from "@/components/TheHeader/TheHeader";
 import MovieSlider from "@/components/TheMovie/MovieSlider";
 import ArticleList from "@/components/TheArticle/ArticleList";
+import { mapActions } from "vuex";
 
 export default {
   name: "CommunityView",
@@ -19,6 +20,7 @@ export default {
     MovieSlider,
   },
   methods: {
+    ...mapActions(["getUserProfile"]),
     inToCommunity() {
       // token 없이 CommunityView에 들어간다면 HomeView로 이동
       if (this.$store.state.token) {
@@ -30,6 +32,7 @@ export default {
   },
   created() {
     this.inToCommunity();
+    this.getUserProfile();
   },
 };
 </script>
--- final-pjt-front/src/views/NotFound404View.vue
+++ final-pjt-front/src/views/NotFound404View.vue
@@ -1,18 +1,55 @@
 <template>
-  <div>
-    <div>
-      <h1>404 Not Found</h1>
-      <router-link :to="{ name: 'HomeView' }">Go To Home</router-link>
-    </div>
+  <div id="content">
+    <div id="canvas"></div>
 
-    <img src="@/assets/images/black-panther.webp" width="100%" />
+    <div id="water-ripples">
+      <div class="go-home">
+        <h1>Not Found 404</h1>
+        <p @click="goToHome">Go Back To Home</p>
+      </div>
+      <img
+        class="backImages"
+        :src="require(`@/assets/images/${movies[0]}`)"
+        alt="Photo by David Pisnoy on Unsplash"
+        crossorigin
+        data-sampler="planeTexture"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "NotFound404View",
+  data() {
+    return {
+      movies: [
+        "black-adam.webp",
+        "black-panther.webp",
+        "everything-everywhere-all-at-once.jpeg",
+        "Hobbit.jpg",
+      ],
+      slideIndex: 1,
+    };
+  },
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "HomeView" });
+      location.reload;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  margin: 0;
+}
+.go-home {
+  height: 200px;
+  z-index: 10;
+  margin: auto auto;
+  align-items: center;
+  opacity: 1;
+}
+</style>
